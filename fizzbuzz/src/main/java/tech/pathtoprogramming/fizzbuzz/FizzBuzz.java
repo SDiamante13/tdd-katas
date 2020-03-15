171cf480a45a83669029bb320869c29bd7c7c7ba
Steven Diamante <pair+stevendiamante13@gmail.com@>
2020-03-15 08:42:26 -0500

    Add module for FizzBuzz. Finished writing test and code.

---
+++ fizzbuzz/src/main/java/tech/pathtoprogramming/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,14 @@
+package tech.pathtoprogramming.fizzbuzz;
+
+public class FizzBuzz {
+
+    public String calculate(int number) {
+        if (number % 3 == 0 && number % 5 == 0)
+            return "FizzBuzz";
+        if (number % 3 == 0)
+            return "Fizz";
+        if (number % 5 == 0)
+            return "Buzz";
+        return String.valueOf(number);
+    }
+}
---
+++ fizzbuzz/src/test/java/tech/pathtoprogramming/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,73 @@
+package tech.pathtoprogramming.fizzbuzz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+//    But for multiples of three print “Fizz” instead of the number and for
+//    the multiples of five print “Buzz”. For numbers which are multiples
+//    of both three and five print “FizzBuzz “.
+
+    private FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    void calculate_shouldReturnFizz_whenNumberIsAMultipleOfThree() {
+        // Arrange
+        String expected = "Fizz";
+
+        // Act
+        String result = fizzBuzz.calculate(9);
+
+        // Assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void calculate_shouldReturnBuzz_whenNumberIsAMultipleOfFive() {
+        // Arrange
+        String expected = "Buzz";
+
+        // Act
+        String result = fizzBuzz.calculate(10);
+
+        // Assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void calculate_shouldReturnFizzBuzz_whenNumberIsAMultipleOfThreeAndFive() {
+        // Arrange
+        String expected = "FizzBuzz";
+
+        // Act
+        String result = fizzBuzz.calculate(15);
+
+        // Assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void calculate_shouldReturnTheNumber_whenNumberIsANotAMultipleOfThreeOrFive() {
+        // Arrange
+        String expected = "4";
+
+        // Act
+        String result = fizzBuzz.calculate(4);
+
+        // Assert
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void calculate_shouldReturnTheFizz_whenNumberIsAMultipleOfThree_butIsNegative() {
+        // Arrange
+        String expected = "Fizz";
+
+        // Act
+        String result = fizzBuzz.calculate(-27);
+
+        // Assert
+        Assertions.assertEquals(expected, result);
+    }
+}
