c81f48230f6c2000afc8ed39dd39435726ee5b58
Steven Diamante <StevenDiamante13@gmail.com>
2021-05-09 23:43:26 -0500

    refactor step: add pointsMap to make adding the score easier

--- tennis/src/main/java/tech/pathtoprogramming/tennis/TennisGame.java
+++ tennis/src/main/java/tech/pathtoprogramming/tennis/TennisGame.java
@@ -1,23 +1,32 @@
 package tech.pathtoprogramming.tennis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TennisGame {
 
     private int player1Score;
     private int player2Score;
+    private Map<Integer, Integer> pointsMap;
 
     public TennisGame() {
         this.player1Score = 0;
         this.player2Score = 0;
+        pointsMap = new HashMap<>();
+        pointsMap.put(0, 0);
+        pointsMap.put(1, 15);
+        pointsMap.put(2, 30);
+        pointsMap.put(3, 40);
     }
 
     public String getScore() {
-        return player1Score + "-" + player2Score;
+        return pointsMap.get(player1Score) + "-" + pointsMap.get(player2Score);
     }
 
     public String score(String playerWhoScored) {
         if (playerWhoScored.equals("Player 1")) {
-          player1Score += 15;
-          return getScore();
+            player1Score++;
+            return getScore();
         } else
             return "0-15";
     }
