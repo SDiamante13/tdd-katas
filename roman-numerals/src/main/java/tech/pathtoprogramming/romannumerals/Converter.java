42da814694f15c0fe7f1a5a68a1616642861c3b5
Steven Diamante <stevendiamante13@gmail.com>
2022-01-11 15:37:24 -0500

    Done with pairing with Eric. Left edge cases. Having trouble fitting IV special case into current design.

--- roman-numerals/src/main/java/tech/pathtoprogramming/romannumerals/Converter.java
+++ roman-numerals/src/main/java/tech/pathtoprogramming/romannumerals/Converter.java
@@ -2,4 +2,30 @@ package tech.pathtoprogramming.romannumerals;
 
 public class Converter {
 
+    public int convertToDecimal(String romanNumeral) {
+        int total = 0;
+        for (char romanLetter : romanNumeral.toCharArray()) {
+            total += convertLetterToDecimal(romanLetter);
+        }
+        return total;
+    }
+
+    private int convertLetterToDecimal(char romanNumeral) {
+        if (romanNumeral == 'I') {
+            return 1;
+        } else if (romanNumeral == 'V') {
+            return 5;
+        } else if (romanNumeral == 'X') {
+            return 10;
+        } else if (romanNumeral == 'L') {
+            return 50;
+        } else if (romanNumeral == 'C') {
+            return 100;
+        } else if (romanNumeral == 'D') {
+            return 500;
+        } else if (romanNumeral == 'M') {
+            return 1000;
+        }
+        return 0;
+    }
 }
--- roman-numerals/src/test/java/tech/pathtotprogramming/romannumerals/ConverterTest.java
+++ roman-numerals/src/test/java/tech/pathtotprogramming/romannumerals/ConverterTest.java
@@ -1,5 +1,30 @@
 package tech.pathtotprogramming.romannumerals;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tech.pathtoprogramming.romannumerals.Converter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ConverterTest {
 
+    private final Converter converter = new Converter();
+
+    @ParameterizedTest
+    @CsvSource({
+            "I,1",
+            "V,5",
+            "X,10",
+            "L,50",
+            "C,100",
+            "D,500",
+            "M,1000",
+            "II,2",
+            "MCCCXXXVII,1337"
+    })
+    void canConvertRomanNumeralToDecimal(String input, int expected) {
+        int result = converter.convertToDecimal(input);
+
+        assertThat(result).isEqualTo(expected);
+    }
 }
