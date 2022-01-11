7075119f51911e3a02d516fab1ce33ffb653836f
Steven Diamante <stevendiamante13@gmail.com>
2022-01-11 14:40:07 -0500

    Clearing out Roman Numeral class. To pair with Eric.

--- roman-numerals/src/test/java/tech/pathtotprogramming/romannumerals/ConverterTest.java
+++ roman-numerals/src/test/java/tech/pathtotprogramming/romannumerals/ConverterTest.java
@@ -1,31 +1,5 @@
 package tech.pathtotprogramming.romannumerals;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import tech.pathtoprogramming.romannumerals.Converter;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 class ConverterTest {
 
-    private final Converter converter = new Converter();
-
-    @ParameterizedTest
-    @CsvSource({
-            "1,I",
-            "2,II",
-            "4,IV",
-            "5,V",
-            "7,VII",
-            "10,X",
-            "12,XII",
-            "18,XVIII",
-            "30,XXX",
-            "2687,MMDCLXXXVII"
-    })
-    void convertToRomanNumeral_returnsRomanNumeral_whenValidInputIsProvided(int input, String expected) {
-        String result = converter.convertToRomanNumeral(input);
-
-        assertThat(result).isEqualTo(expected);
-    }
 }
