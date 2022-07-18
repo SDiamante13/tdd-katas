f0a1a5b378d8424f1ab03ac14b833808b8f0e3b2
Steven Diamante <stevendiamante13@gmail.com>
2022-07-18 00:36:37 -0400

    Refactor: Further clean up

--- shopping-basket/src/test/java/tech/pathtoprogramming/shoppingbasket/AShoppingBasketShould.java
+++ shopping-basket/src/test/java/tech/pathtoprogramming/shoppingbasket/AShoppingBasketShould.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static tech.pathtoprogramming.shoppingbasket.Items.*;
 
 // test list:
 
@@ -14,46 +15,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 // zero items
 // one item $50
 // 1 item of $25, 2 items of $ 49
-//
 
 
 class AShoppingBasketShould {
 
-    private static final Map<String, Integer> NO_ITEMS = Map.of();
-    public static final HashMap<String, Integer> ONE_ITEM = new HashMap<>() {{
-        put("eggs", 50);
-    }};
-
-
-    public static final HashMap<String, Integer> one = new HashMap<>() {{
-        put("eggs", 50);
-    }};
-
     @Test
-    void haveZeroTotalForNoItems() {
-        float actualTotalPrice = new ShoppingBasket(Items.of()).calculateTotalPrice();
+    void haveZeroTotalPriceForNoItems() {
+        float actualTotalPrice = new ShoppingBasket(of()).calculateTotalPrice();
 
         assertThat(actualTotalPrice).isZero();
     }
 
     @Test
     void calculateTotalPriceOfOneItem() {
-        float actualTotalPrice = new ShoppingBasket(
-                Items.of(
-                        new Item("eggs", 50)
-                ))
-                .calculateTotalPrice();
+        float actualTotalPrice = new ShoppingBasket(of(
+                new Item("eggs", 50)
+        )).calculateTotalPrice();
 
         assertThat(actualTotalPrice).isEqualTo(50);
     }
 
     @Test
-    void twoItems() {
-        float actualTotalPrice = new ShoppingBasket(Items.of(new Item("eggs", 50), new Item("milk", 3))).calculateTotalPrice();
+    void calculateTotalPriceOfTwoItems() {
+        float actualTotalPrice = new ShoppingBasket(of(
+                new Item("eggs", 50),
+                new Item("milk", 3)
+        )).calculateTotalPrice();
 
         assertThat(actualTotalPrice).isEqualTo(50 + 3);
-
     }
+
 
 }
 
@@ -83,10 +74,6 @@ class Items {
             this.items.put(newItem.getName(), newItem);
         }
     }
-    // wrote too much code here. The get is not necessary since items calculate its own total
-//    public Item get(String name) {
-//        return items.get(name);
-//    }
 
     public float total() {
         return items.values().stream()
