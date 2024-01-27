23fa2cfb8295242b7dec9a5ff81219054ead5cd1
Steven Diamante <stevendiamante13@gmail.com>
2024-01-27 11:14:50 -0500

    Add Promotion Service Feature Envy Kata

---
+++ promotion-service/src/test/java/promotionservice/PromotionServiceTest.java
@@ -0,0 +1,44 @@
+package promotionservice;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PromotionServiceTest {
+
+    private PromotionService promotionService = new PromotionService();
+
+    @Test
+    void bookPromotion() {
+        List<UserMessage> messages = promotionService.applyPromotionTo(
+                new Item(
+                        "Functional programming with C++",
+                        10,
+                        0.2
+                )
+        );
+
+        assertThat(messages).containsExactly(
+                new UserMessage("Total before promotion: 12.0"),
+                new UserMessage("Total after promotion: 9.6")
+        );
+    }
+
+    @Test
+    void expensiveBookPromotion() {
+        List<UserMessage> messages = promotionService.applyPromotionTo(
+                new Item(
+                        "Functional programming with all languages",
+                        210,
+                        0.2
+                )
+        );
+
+        assertThat(messages).containsExactly(
+                new UserMessage("Total before promotion: 252.0"),
+                new UserMessage("Total after promotion: 228.8")
+        );
+    }
+}
