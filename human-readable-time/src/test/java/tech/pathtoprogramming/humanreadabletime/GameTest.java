d3585ee5d81196f7017e9deab1a99d96bb3559fa
Steven Diamante <stevendiamante13@gmail.com>
2024-05-04 18:55:13 -0400

    Bowling refactor

---
+++ human-readable-time/src/test/java/tech/pathtoprogramming/humanreadabletime/GameTest.java
@@ -0,0 +1,77 @@
+package tech.pathtoprogramming.humanreadabletime;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class GameTest {
+
+    @Test
+    void fullGame() {
+        Game game = new Game();
+
+        game.rolls(5,5);
+    }
+}
+class Game {
+    private int score = 0;
+    private int remaining = 10;
+    private boolean spare = false;
+    private boolean strike = false;
+    private int bonusRolls = 0;
+    private int frames = 0;
+    private int frameRolls = 0;
+
+    public int score() {
+        return score;
+    }
+
+    public void roll(int pins) {
+        if(frames > 9){
+            if(frameRolls >= 2){
+                if(!(spare || strike)){
+                    return;
+                }
+            }
+        }
+
+        frameRolls++;
+
+        if(strike){
+            score += pins;
+            bonusRolls++;
+            if(bonusRolls == 2){
+                strike = false;
+                bonusRolls = 0;
+            }
+        }
+
+        if(spare) {
+            score += pins;
+            spare = false;
+        }
+
+        score += pins;
+        remaining -= pins;
+
+        strike = pins == 10 || bonusRolls > 0;
+        spare = remaining == 0 && !strike;
+
+        if(remaining == 0 || frameRolls == 2){
+            remaining = 10;
+            frames++;
+            if(frames < 10) {
+                frameRolls = 0;
+            }
+        }
+
+    }
+
+    public int getFrameCount() {
+        return frames;
+    }
+
+    void rolls(int... rolls) {
+        Arrays.stream(rolls).forEach((pins) -> roll(pins));
+    }
+}
