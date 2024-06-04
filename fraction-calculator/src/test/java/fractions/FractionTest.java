9e9ba847d3021cbc80435e10193c5f50f3cc7023
Steven Diamante <stevendiamante13@gmail.com>
2024-06-04 00:30:12 -0400

    Fractions: r - convert to record.

--- fraction-calculator/src/test/java/fractions/FractionTest.java
+++ fraction-calculator/src/test/java/fractions/FractionTest.java
@@ -25,45 +25,11 @@ class FractionTest {
     }
 }
 
-class Fraction {
-
-    private final int numerator;
-    private final int denominator;
-
-    public Fraction(int numerator, int denominator) {
-        this.numerator = numerator;
-        this.denominator = denominator;
-    }
-
-    public int numerator() {
-        return numerator;
-    }
-
-    public int denominator() {
-        return denominator;
-    }
+record Fraction(int numerator, int denominator) {
 
     public Fraction plus(Fraction fraction) {
         int sumOfNumerators = numerator + fraction.numerator();
         return new Fraction(sumOfNumerators, 1);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Fraction fraction = (Fraction) o;
-
-        if (numerator != fraction.numerator) return false;
-        return denominator == fraction.denominator;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = numerator;
-        result = 31 * result + denominator;
-        return result;
     }
 
     @Override
