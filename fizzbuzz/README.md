b2f00dd9edbfeecf20f7d2dac0184efb06e41e06
Steven Diamante <stevendiamante13@gmail.com>
2024-06-03 22:23:31 -0400

    Complete extreme fake it til you make it with fizz buzz.

---
+++ fizzbuzz/README.md
@@ -0,0 +1,7 @@
+# Extreme Fake it Till You Make it
+
+1. Separate
+2. Encapsulate
+3. Calculate
+4. Automate
+5. Clean
--- fizzbuzz/src/test/java/tech/pathtoprogramming/fizzbuzz/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/tech/pathtoprogramming/fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,37 @@
 package tech.pathtoprogramming.fizzbuzz;
 
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class FizzBuzzTest {
 
+    @Test
+    void countTo20() {
+        String result = FizzBuzz.countTo(20);
+        Approvals.verify(result);
+    }
+
+    private static class FizzBuzz {
+        public static String countTo(int lastNumber) {
+            List<String> parts = new ArrayList<>();
+            for (int i = 1; i <= lastNumber; i++) {
+                parts.add(calculate(i));
+            }
+            return String.join("\n", parts);
+        }
+
+        private static String calculate(int i) {
+            if (i % 15 == 0) {
+                return "FizzBuzz";
+            } else if (i % 3 == 0) {
+                return "Fizz";
+            } else if (i % 5 == 0) {
+                return "Buzz";
+            }
+            return String.valueOf(i);
+        }
+    }
 }
