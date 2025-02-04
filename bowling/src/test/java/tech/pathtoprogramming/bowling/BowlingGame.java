9224bbc6c423b50979635d9005858f810910fc73
Steven Diamante <stevendiamante13@gmail.com>
2025-02-04 10:16:13 -0500

    Fix compile error.

--- bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGame.java
+++ bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGame.java
@@ -1,13 +1,29 @@
 package tech.pathtoprogramming.bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
-    private int score;
+    private List<Integer> frames = new ArrayList<>();
 
     public void roll(int pins) {
-        score += pins;
+        if (lastTwoFramesAddUpToTen()) {
+            int index = frames.size() - 1;
+            int currentValue = frames.get(index);
+            frames.set(index, currentValue + pins);
+        }
+        frames.add(pins);
+    }
+
+    private boolean lastTwoFramesAddUpToTen() {
+        int index = frames.size() - 1;
+        return frames.size() >= 2 &&
+                frames.get(index) + frames.get(index - 1) == 10;
     }
 
     public int score() {
-        return score;
+        return frames.stream()
+                .mapToInt(i -> i)
+                .sum();
     }
 }
--- bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGameTest.java
+++ bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGameTest.java
@@ -1,15 +1,12 @@
 package tech.pathtoprogramming.bowling;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import java.util.Calendar;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 // [x] - gutter balls
 // [x] - all ones
-// [ ] - all 9's
+// [x] - all 4's
 // [ ] - mostly ones with spare at start
 // [ ] - mostly ones with strike at start
 // [ ] - perfect game?
@@ -19,17 +16,33 @@ class BowlingGameTest {
 
     @Test
     void allZeroes() {
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        rollMultiple(0, 20);
         assertThat(game.score()).isZero();
     }
 
     @Test
     void allOnes() {
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        rollMultiple(1, 20);
         assertThat(game.score()).isEqualTo(20);
+    }
+
+    @Test
+    void allFours() {
+        rollMultiple(4, 20);
+        assertThat(game.score()).isEqualTo(80);
+    }
+
+    @Test
+    void spare() {
+        game.roll(9);
+        game.roll(1);
+        rollMultiple(1, 18);
+        assertThat(game.score()).isEqualTo(29);
+    }
+
+    private void rollMultiple(int pins, int times) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pins);
+        }
     }
 }
