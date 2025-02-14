00fcc024089e97ac38da00d2c2d22c911f3b5ce5
Steven Diamante <stevendiamante13@gmail.com>
2025-02-14 12:59:05 -0500

    . f handles single 5 in one case

--- greed-kata/src/main/java/com/dtc/greed/Roll.java
+++ greed-kata/src/main/java/com/dtc/greed/Roll.java
@@ -2,6 +2,9 @@ package com.dtc.greed;
 
 record Roll(int die1, int die2, int die3, int die4, int die5) {
     public Points calculatePoints() {
+        if (die5 == 5) {
+            return new Points(50);
+        }
         return new Points(0);
     }
 }
--- greed-kata/src/test/java/com/dtc/greed/PointCalculationTests.java
+++ greed-kata/src/test/java/com/dtc/greed/PointCalculationTests.java
@@ -1,6 +1,5 @@
 package com.dtc.greed;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
