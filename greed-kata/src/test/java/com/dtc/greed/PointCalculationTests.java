7faa2031bdcf236877ec69043f399bcc81b4b5a9
Steven Diamante <stevendiamante13@gmail.com>
2025-02-14 13:08:16 -0500

    . t rewrite as parameterized test. Expand to cover single 5 in all places.

--- greed-kata/src/test/java/com/dtc/greed/PointCalculationTests.java
+++ greed-kata/src/test/java/com/dtc/greed/PointCalculationTests.java
@@ -1,6 +1,8 @@
 package com.dtc.greed;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,13 +18,20 @@ class PointCalculationTests {
                 .isEqualTo(new Points(0));
     }
 
-    @Test
-    void name() {
-        Roll roll = new Roll(2, 3, 4, 6, 5);
+    @ParameterizedTest
+    @CsvSource({
+            "2,3,4,6,5,50",
+            "2,3,4,5,6,50",
+            "2,3,5,4,6,50",
+            "2,5,3,4,6,50",
+            "5,2,3,4,6,50"
+    })
+    void fiftyPointsForSingleFive(int die1, int die2, int die3, int die4, int die5, int expectedPointsValue) {
+        Roll roll = new Roll(die1, die2, die3, die4, die5);
 
         Points actualPoints = roll.calculatePoints();
 
         assertThat(actualPoints)
-                .isEqualTo(new Points(50));
+                .isEqualTo(new Points(expectedPointsValue));
     }
 }
