81398186f817745228ef6ff6afe26b993aebc342
Steven Diamante <stevendiamante13@gmail.com>
2025-02-14 13:08:54 -0500

    . f calculates single five in any die place

--- greed-kata/src/main/java/com/dtc/greed/Roll.java
+++ greed-kata/src/main/java/com/dtc/greed/Roll.java
@@ -1,8 +1,11 @@
 package com.dtc.greed;
 
+import java.util.stream.IntStream;
+
 record Roll(int die1, int die2, int die3, int die4, int die5) {
     public Points calculatePoints() {
-        if (die5 == 5) {
+        IntStream diceStream = IntStream.of(die1, die2, die3, die4, die5);
+        if (diceStream.anyMatch(value -> value == 5)) {
             return new Points(50);
         }
         return new Points(0);
