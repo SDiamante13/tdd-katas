ad59f14e738ffaae586d7a82886556a01830eb98
Steven Diamante <stevendiamante13@gmail.com>
2025-02-14 13:47:51 -0500

    . r make algorithms look similar

--- greed-kata/src/main/java/com/dtc/greed/Roll.java
+++ greed-kata/src/main/java/com/dtc/greed/Roll.java
@@ -3,20 +3,30 @@ package com.dtc.greed;
 import java.util.Arrays;
 
 record Roll(int die1, int die2, int die3, int die4, int die5) {
+    // introduce Dice!
+    // ask Dice the questions you have.
+    // or ScoringRuleInterface
+
+
     public Points calculatePoints() {
         int[] dice = {die1, die2, die3, die4, die5};
-        int sum = Arrays.stream(dice)
+
+        int sum = 0;
+
+        int numberOfFives = (int) Arrays.stream(dice)
                 .filter(d -> d == 5)
-                .map(d -> 50)
-                .sum();
-        sum += Arrays.stream(dice)
+                .count();
+        sum += numberOfFives * 50;
+
+        int numberOfOnes = (int) Arrays.stream(dice)
                 .filter(d -> d == 1)
-                .map(d -> 100)
-                .sum();
+                .count();
+        sum += numberOfOnes * 100;
+
         long count = Arrays.stream(dice)
                 .filter(d -> d == 2)
                 .count();
-        sum += count == 3 ? 200 : 0;
+        sum += count == 3 ? 200 : 0; // expression
 
         return new Points(sum);
     }
