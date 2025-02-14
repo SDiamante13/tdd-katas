bdf4d1a4c8fe6347763d311c55404246d9572c39
Steven Diamante <stevendiamante13@gmail.com>
2025-02-14 13:17:44 -0500

    . r switch from stream to array for simplicity for now

--- greed-kata/src/main/java/com/dtc/greed/Roll.java
+++ greed-kata/src/main/java/com/dtc/greed/Roll.java
@@ -1,11 +1,10 @@
 package com.dtc.greed;
 
-import java.util.stream.IntStream;
-
 record Roll(int die1, int die2, int die3, int die4, int die5) {
     public Points calculatePoints() {
-        IntStream diceStream = IntStream.of(die1, die2, die3, die4, die5);
-        if (diceStream.anyMatch(value -> value == 5)) {
+        int[] dice = {die1, die2, die3, die4, die5};
+
+        if (dice[0] == 5 || dice[1] == 5 || dice[2] == 5 || dice[3] == 5 || dice[4] == 5) {
             return new Points(50);
         }
         return new Points(0);
