ff4b306ba5fa633a444bd1b84f2bada8fb7714bb
Steven Diamante <stevendiamante13@gmail.com>
2025-02-14 12:53:42 -0500

    . t calculate score of roll including single 5.

--- greed-kata/src/test/java/com/dtc/greed/PointCalculationTests.java
+++ greed-kata/src/test/java/com/dtc/greed/PointCalculationTests.java
@@ -1,5 +1,6 @@
 package com.dtc.greed;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,5 +15,15 @@ class PointCalculationTests {
 
         assertThat(actualPoints)
                 .isEqualTo(new Points(0));
+    }
+
+    @Test
+    void name() {
+        Roll roll = new Roll(2, 3, 4, 6, 5);
+
+        Points actualPoints = roll.calculatePoints();
+
+        assertThat(actualPoints)
+                .isEqualTo(new Points(50));
     }
 }
