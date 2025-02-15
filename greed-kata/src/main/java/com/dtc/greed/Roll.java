cafe5fea994605310b46e2decf36846d65dceebd
Steven Diamante <stevendiamante13@gmail.com>
2025-02-14 21:03:09 -0500

    . r use custom constructor for Roll

--- greed-kata/src/main/java/com/dtc/greed/Roll.java
+++ greed-kata/src/main/java/com/dtc/greed/Roll.java
@@ -2,18 +2,24 @@ package com.dtc.greed;
 
 import java.util.List;
 
-record Roll(int die1, int die2, int die3, int die4, int die5) {
+final class Roll {
 
-    private static final List<ScoringRule> scoringRules = List.of(new FivesScoringRule(), new OnesScoringRule(), new TripletTwosScoringRule());
+    private static final List<ScoringRule> scoringRules = List.of(
+            new OnesScoringRule(),
+            new FivesScoringRule(),
+            new TripletTwosScoringRule()
+    );
+
+    private final Dice dice;
+
+    Roll(int die1, int die2, int die3, int die4, int die5) {
+        this.dice = new Dice(die1, die2, die3, die4, die5);
+    }
 
     public Points calculatePoints() {
-        Dice dice = new Dice(die1, die2, die3, die4, die5);
-
         int sum = scoringRules.stream()
                 .mapToInt(a -> a.calculateScore(dice))
                 .sum();
-
         return new Points(sum);
     }
-
 }
