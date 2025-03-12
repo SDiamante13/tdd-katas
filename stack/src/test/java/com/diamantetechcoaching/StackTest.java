9bbe3691be32514096cd9a80db205113104dcfd6
Steven Diamante <stevendiamante13@gmail.com>
2025-03-12 15:07:21 -0400

    ^ F Stack can hold one element is not empty.

--- stack/src/test/java/com/diamantetechcoaching/StackTest.java
+++ stack/src/test/java/com/diamantetechcoaching/StackTest.java
@@ -10,10 +10,11 @@ class StackTest {
     Test List
     ---------
     [x] - new stack should be empty
-    [ ] - stack with one element should not be empty
+    [x] - stack with one element should not be empty
     [ ] - stack with two elements has size of 2
     [ ] - peek returns the last added element without removing it
     [ ] - pop returns the last element that was pushed
+    [ ] - Stack handles generics
     */
     @Test
     void newStackIsEmpty() {
@@ -23,9 +24,27 @@ class StackTest {
                 .isTrue();
     }
 
+    @Test
+    void stackWithOneElementIsNotEmpty() {
+        Stack stack = new Stack();
+        stack.push(1);
+
+        assertThat(stack.isEmpty()).isFalse();
+    }
+
     private static class Stack {
+
+        private int element = -1;
+
         public boolean isEmpty() {
+            if (element != -1) {
+                return false;
+            }
             return true;
+        }
+
+        public void push(int element) {
+            this.element = element;
         }
     }
 }
