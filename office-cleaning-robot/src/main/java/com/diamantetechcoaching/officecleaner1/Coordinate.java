c9cbf8b3b9911247d7ba0ee469ebb6d6624663d0
Steven Diamante <stevendiamante13@gmail.com>
2025-03-14 11:32:04 -0400

    Add Office Cleaning Robot.

---
+++ office-cleaning-robot/src/main/java/com/diamantetechcoaching/officecleaner1/Coordinate.java
@@ -0,0 +1,18 @@
+package com.diamantetechcoaching.officecleaner1;
+
+// A class to keep track of the X and Y positions
+public class Coordinate {
+    public int x;
+    public int y;
+
+    public Coordinate(int horiz, int vert) {
+        x = horiz;
+        y = vert;
+    }
+
+    @Override
+    public String toString() {
+        return x + "," + y;
+    }
+}
+
