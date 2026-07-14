d58e0ce404741f0bcf6b7d36c8a4df37c2c825ce
Steven Diamante <stevendiamante13@gmail.com>
2026-07-14 19:25:53 -0400

    Refactor bowling rolls into a list

--- bowling/src/main/java/tech/pathtoprogramming/bowling/BowlingGame.java
+++ bowling/src/main/java/tech/pathtoprogramming/bowling/BowlingGame.java
@@ -1,29 +1,39 @@
 package tech.pathtoprogramming.bowling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
 
-    private final int[] rolls = new int[21];
-    private int rollCount;
+    private final List<Integer> rolls = new ArrayList<>();
 
     public void roll(int pins) {
-        rolls[rollCount++] = pins;
+        rolls.add(pins);
     }
 
     public int score() {
         int total = 0;
         int roll = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (rolls[roll] == 10) {
-                total += 10 + rolls[roll + 1] + rolls[roll + 2];
+            if (isStrike(roll)) {
+                total += 10 + rolls.get(roll + 1) + rolls.get(roll + 2);
                 roll++;
-            } else if (rolls[roll] + rolls[roll + 1] == 10) {
-                total += 10 + rolls[roll + 2];
+            } else if (isSpare(roll)) {
+                total += 10 + rolls.get(roll + 2);
                 roll += 2;
             } else {
-                total += rolls[roll] + rolls[roll + 1];
+                total += rolls.get(roll) + rolls.get(roll + 1);
                 roll += 2;
             }
         }
         return total;
+    }
+
+    private boolean isStrike(int roll) {
+        return rolls.get(roll) == 10;
+    }
+
+    private boolean isSpare(int roll) {
+        return rolls.get(roll) + rolls.get(roll + 1) == 10;
     }
 }
