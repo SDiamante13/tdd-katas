6781ca12f6b7385f8024cd9087f846d463dd2b8b
Steven Diamante <stevendiamante13@gmail.com>
2026-07-14 16:28:57 -0400

    reset bowling game kata

--- bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGame.java
+++ bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGame.java
@@ -1,29 +1,5 @@
 package tech.pathtoprogramming.bowling;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BowlingGame {
-    private List<Integer> frames = new ArrayList<>();
 
-    public void roll(int pins) {
-        if (lastTwoFramesAddUpToTen()) {
-            int index = frames.size() - 1;
-            int currentValue = frames.get(index);
-            frames.set(index, currentValue + pins);
-        }
-        frames.add(pins);
-    }
-
-    private boolean lastTwoFramesAddUpToTen() {
-        int index = frames.size() - 1;
-        return frames.size() >= 2 &&
-                frames.get(index) + frames.get(index - 1) == 10;
-    }
-
-    public int score() {
-        return frames.stream()
-                .mapToInt(i -> i)
-                .sum();
-    }
 }
--- bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGameTest.java
+++ bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGameTest.java
@@ -4,45 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-// [x] - gutter balls
-// [x] - all ones
-// [x] - all 4's
-// [ ] - mostly ones with spare at start
-// [ ] - mostly ones with strike at start
-// [ ] - perfect game?
 class BowlingGameTest {
 
-    private final BowlingGame game = new BowlingGame();
-
-    @Test
-    void allZeroes() {
-        rollMultiple(0, 20);
-        assertThat(game.score()).isZero();
-    }
-
-    @Test
-    void allOnes() {
-        rollMultiple(1, 20);
-        assertThat(game.score()).isEqualTo(20);
-    }
-
-    @Test
-    void allFours() {
-        rollMultiple(4, 20);
-        assertThat(game.score()).isEqualTo(80);
-    }
-
-    @Test
-    void spare() {
-        game.roll(9);
-        game.roll(1);
-        rollMultiple(1, 18);
-        assertThat(game.score()).isEqualTo(29);
-    }
-
-    private void rollMultiple(int pins, int times) {
-        for (int i = 0; i < times; i++) {
-            game.roll(pins);
-        }
-    }
 }
