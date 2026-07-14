4fac5e7dbd6cf9162153b658196cbf57866b4731
Steven Diamante <stevendiamante13@gmail.com>
2026-07-14 19:15:08 -0400

    Revert "Add gutter game scoring behavior"
    This reverts commit 44e2ad4d042c7cc40a5d1e1b1fecbadc1f11bf6f.

--- bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGame.java
+++ bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGame.java
@@ -2,13 +2,4 @@ package tech.pathtoprogramming.bowling;
 
 public class BowlingGame {
 
-    private int score;
-
-    public void roll(int pins) {
-        score += pins;
-    }
-
-    public int score() {
-        return score;
-    }
 }
--- bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGameTest.java
+++ bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGameTest.java
@@ -6,14 +6,4 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BowlingGameTest {
 
-    @Test
-    void aGutterGameScoresZero() {
-        BowlingGame game = new BowlingGame();
-
-        for (int roll = 0; roll < 20; roll++) {
-            game.roll(0);
-        }
-
-        assertThat(game.score()).isZero();
-    }
 }
