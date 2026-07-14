20843686c789034d2db978cfc8de86cdff99b2f2
Steven Diamante <stevendiamante13@gmail.com>
2026-07-14 19:17:18 -0400

    Score a gutter game

--- bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGame.java
+++ bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGame.java
@@ -2,4 +2,10 @@ package tech.pathtoprogramming.bowling;
 
 public class BowlingGame {
 
+    public void roll(int pins) {
+    }
+
+    public int score() {
+        return 0;
+    }
 }
--- bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGameTest.java
+++ bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGameTest.java
@@ -6,4 +6,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BowlingGameTest {
 
+    @Test
+    void scoresAGutterGame() {
+        BowlingGame game = new BowlingGame();
+        rollMany(game, 20, 0);
+
+        assertThat(game.score()).isEqualTo(0);
+    }
+
+    private void rollMany(BowlingGame game, int rolls, int pins) {
+        for (int roll = 0; roll < rolls; roll++) {
+            game.roll(pins);
+        }
+    }
 }
