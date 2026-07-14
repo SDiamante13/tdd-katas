44e2ad4d042c7cc40a5d1e1b1fecbadc1f11bf6f
Steven Diamante <stevendiamante13@gmail.com>
2026-07-14 19:10:59 -0400

    Add gutter game scoring behavior

--- bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGameTest.java
+++ bowling/src/test/java/tech/pathtoprogramming/bowling/BowlingGameTest.java
@@ -6,4 +6,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BowlingGameTest {
 
+    @Test
+    void aGutterGameScoresZero() {
+        BowlingGame game = new BowlingGame();
+
+        for (int roll = 0; roll < 20; roll++) {
+            game.roll(0);
+        }
+
+        assertThat(game.score()).isZero();
+    }
 }
